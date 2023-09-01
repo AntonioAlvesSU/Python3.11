3ca3ae914572208bf407889ea82e0192d79e0ee0
Antonio Alves <aalves_su@hotmail.com>
2023-09-01 14:51:44 -0300

    Automação de mesagens

--- dic.py
+++ dic.py
@@ -255,6 +255,13 @@ print(dic1==dict2)
 dicionario = OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
 for chave, valor in dicionario.items():
     print(f'chave={chave}: valor={valor}')
+"""
+chave=a: valor=1
+chave=b: valor=2
+chave=c: valor=3
+chave=d: valor=4
+chave=e: valor=5
+"""    
 
 
 #print(dir(dic1))
